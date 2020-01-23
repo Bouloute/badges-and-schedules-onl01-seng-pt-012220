d91a54179268a96904d370aa6aa0496935b983b4
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-23 22:12:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,3 +6,10 @@ end
 def batch_badge_creator(names)
   names.collect{|name| badge_maker(name)}
 end
+
+def assign_rooms(names)
+  assigned_people_with_room = []
+  names.each_with_index{|name, index|
+    assigned_people_with_room.push("Hello, #{name}! You'll be assigned to room #{index}!")
+  }
+end
