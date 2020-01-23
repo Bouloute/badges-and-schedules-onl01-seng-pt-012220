368730bac132bb79b47f0a848caf0ee8c8b21c66
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-23 22:13:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ end
 def assign_rooms(names)
   assigned_people_with_room = []
   names.each_with_index{|name, index|
-    assigned_people_with_room.push("Hello, #{name}! You'll be assigned to room #{index}!")
+    assigned_people_with_room.push("Hello, #{name}! You'll be assigned to room #{index + 1}!")
   }
    puts assigned_people_with_room
 end
